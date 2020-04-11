9609dd167250370fedf0dd7da9c07cba178dd729
unknown <makprg@ukr.net>
2020-04-11 19:26:18 +0300

    before pipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -15,6 +16,24 @@ export class TasksService {
         return this.tasks.find(task => task.id === id);
     }
 
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[]{
+        const {status, search} = filterDto;
+
+        let tasks = this.getAllTasks();
+
+        if (status) {
+            tasks = tasks.filter(task => task.status === status);
+        } 
+
+        if (search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search),
+            ); 
+        }
+
+        return tasks;
+    }
 
     createTask(createTaskDto: CreateTaskDto): Task{
         const {title, description} = createTaskDto;
