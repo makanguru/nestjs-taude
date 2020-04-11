e7c659bbb1a76b1c773ee82a7a6d5db2619101ae
unknown <makprg@ukr.net>
2020-04-11 18:26:48 +0300

    PATCH

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -29,24 +29,12 @@ export class TasksController {
         this.tasksService.deleteTask(id);
     }
 
-
-//    @Post()
-//    createTask(
-//        @Body('title') title: string,
-//        @Body('description') description: string
-//    ): Task {
-//        return this.tasksService.createTask(title, description);
-//
-//        // console.log('body', Body);
-//        // console.log('title', title)
-//        // console.log('description', description)
-//    }
-
-    // @Post() 
-    // createTask(@Body() body) {
-    //     console.log(1, 'body', body);
-    // }
-
-
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus
+    ) {
+        return this.tasksService.updateTaskStatus(id, status);
+    }
 
 }
